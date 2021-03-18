cf5342b85015e2ff0a4c7b2e4dd27557b45af17f
LolaChernysheva <lolita-chernysheva@mail.ru>
2021-03-18 18:57:21 +0300

    init

---
+++ MVC-N/MVC-N/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVC-N
+//
+//  Created by Лолита on 18.03.2021.
+//  Copyright © 2021 Lolita Chernysheva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
