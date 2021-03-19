cda565ff9df9c443a0130ad7b82121fba83c419b
LolaChernysheva <lolita-chernysheva@mail.ru>
2021-03-19 11:10:57 +0300

    Добавлена MVC, добавлен файл Comments

---
+++ MVC-N/MVC-N/Comments/Controller/CommentsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  CommentsViewController.swift
+//  MVC-N
+//
+//  Created by Лолита on 18.03.2021.
+//  Copyright © 2021 Lolita Chernysheva. All rights reserved.
+//
+
+import UIKit
+
+class CommentsViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+
+
+}
+
+extension CommentsViewController:  UITableViewDelegate {
+    
+    
+}
+
+extension CommentsViewController:  UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+    
+}
+
+
+
+
