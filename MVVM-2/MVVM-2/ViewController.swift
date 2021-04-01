025b5dfe48fd32718e003685304c7c51e3ed9494
LolaChernysheva <lolita-chernysheva@mail.ru>
2021-04-01 16:43:42 +0300

    Добавлен новый проект по реализации паттерна  MVVM в проектах с TableViewController

---
+++ MVVM-2/MVVM-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM-2
+//
+//  Created by Лолита on 01.04.2021.
+//  Copyright © 2021 Lolita Chernysheva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
